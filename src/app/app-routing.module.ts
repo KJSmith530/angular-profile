e0c3d600312f6a6f2e1458612c745cb0c52bb082
Kyle <xikkzz@gmail.com>
2020-11-11 19:39:26 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: '', redirectTo: '/profile', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', redirectTo: '/profile', pathMatch: 'full' },
+]; // Wildcard route for a 404 page];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/profile.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { UserProfile } from './interfaces/user-profile';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  userProfile: UserProfile = {
+    name: 'Kyle Smith',
+    contact: 'kjsmith530@gmail.com',
+    bio: 'I play video games',
+  };
+  constructor() {}
+
+  getUserProfile = () => {
+    return this.userProfile;
+  };
+
+  setUserProfile = () => {};
+}
